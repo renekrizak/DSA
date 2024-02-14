706c02df94b80a3cb9ad4f31fc9e7def3216b0c7
renekrizak <rene.krizak@gmail.com>
2024-02-14 17:30:30 +0100

    insertion sort

--- Algorithms/InsertionSort.c
+++ Algorithms/InsertionSort.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
+void InsertionSort(int *arr, int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
 int main()
 {
+    int arr[] = {8, 2, 5, 1, 4, 3, 6, 7, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    InsertionSort(arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
     return 0;
 }
